e59e2d98c90f2415fb1043a5596ee1c456dd1a5d
ArlPrima <arielprima@gmail.com>
2023-11-18 19:11:41 +0700

    preparation app

---
+++ lib/tools/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const Color black = Color(0xFF000000);
+const Color blue = Color(0xFF14213D);
+const Color yellow = Color(0xFFFCA311);
+const Color gray = Color(0xFFE5E5E5);
+const Color white = Color(0xFFFFFFFF);
---
+++ lib/tools/style.dart
@@ -0,0 +1,44 @@
+import 'package:bmi/tools/color.dart';
+import 'package:flutter/cupertino.dart';
+
+const TextStyle black36b = TextStyle(
+  color: black,
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle black13b = TextStyle(
+  color: black,
+  fontSize: 13,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle black14sb = TextStyle(
+  color: black,
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle black20sb = TextStyle(
+  color: black,
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle black16m = TextStyle(
+  color: black,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle black12r = TextStyle(
+  color: black,
+  fontSize: 36,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle white16sb = TextStyle(
+  color: white,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
